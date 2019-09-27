91b931a38c64b9ec61b425d04d44f809f55b66fe
1227900499@qq.com <zy18228145045>
2019-09-27 14:50:35 +0800

    项目更新后提交

---
+++ springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApplicationTests {
+
+}
