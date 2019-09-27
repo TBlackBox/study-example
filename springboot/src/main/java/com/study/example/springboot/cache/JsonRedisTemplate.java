54b9b7be5eee8e321a55c429b87f54c11b36904d
1227900499@qq.com <zy18228145045>
2019-09-27 10:15:07 +0800

    update

--- springboot/src/main/java/com/study/example/springboot/cache/JsonRedisTemplate.java
+++
@@ -1,6 +0,0 @@
-package com.study.example.springboot.cache;
-
-import org.springframework.data.redis.core.RedisTemplate;
-
-public class JsonRedisTemplate extends RedisTemplate {
-}
--- springboot/src/test/java/com/study/example/springboot/Test.java
+++
@@ -1,6 +0,0 @@
-package com.study.example.springboot;
-
-
-public class Test {
-
-}
