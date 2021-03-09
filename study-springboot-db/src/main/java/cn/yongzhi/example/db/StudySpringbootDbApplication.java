7cb3561c77749242b45fc991604082f113c503ae
qq <qqq@qq.com>
2021-03-09 17:44:52 +0800

    调整项目结构

---
+++ study-springboot-db/src/main/java/cn/yongzhi/example/db/StudySpringbootDbApplication.java
@@ -0,0 +1,13 @@
+package cn.yongzhi.example.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringbootDbApplication.class, args);
+    }
+
+}
---
+++ study-springboot-db/src/test/java/cn/yongzhi/example/db/StudySpringbootDbApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yongzhi.example.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
