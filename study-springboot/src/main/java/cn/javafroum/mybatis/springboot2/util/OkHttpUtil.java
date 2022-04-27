a7a1bb7416fd575b8aef47640e7b1b0b44d788fd
SC-202103121651\Administrator <1227900499@qq.com>
2022-04-27 18:33:35 +0800

    update

--- study-springboot/src/main/java/cn/javafroum/mybatis/springboot2/util/OkHttpUtil.java
+++ study-springboot/src/main/java/cn/javafroum/mybatis/springboot2/util/OkHttpUtil.java
@@ -1,18 +1,17 @@
 package cn.javafroum.mybatis.springboot2.util;
-import java.io.IOException;
-import java.util.Map;
 
+import okhttp3.*;
+import okhttp3.internal.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import okhttp3.FormBody;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Map;
 
 @Component
 public class OkHttpUtil{
@@ -219,4 +218,50 @@ public class OkHttpUtil{
         }
         return "";
     }
+
+    private static String doPostFile() {
+        HttpURLConnection url_con = null;
+        String responseContent = null;
+        try {
+            URL url = new URL("http://192.168.0.115:24409/?threshold=0");
+            url_con = (HttpURLConnection) url.openConnection();
+            url_con.setRequestMethod("POST");
+            url_con.setDoOutput(true);
+            url_con.setRequestProperty("Content-type", "application/x-java-serialized-object");
+            File file = new File("D:\\work\\code\\qtstudy\\test1.png");
+            byte[] data = Files.readAllBytes(file.toPath());
+//            byte[] data = Util.readFileByBytes(fileUrl);
+            url_con.getOutputStream().write(data, 0, data.length);
+            url_con.getOutputStream().flush();
+            url_con.getOutputStream().close();
+
+            InputStream in = url_con.getInputStream();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String tempLine = rd.readLine();
+            StringBuffer tempStr = new StringBuffer();
+            String crlf = System.getProperty("line.separator");
+            while (tempLine != null) {
+                tempStr.append(tempLine);
+                tempStr.append(crlf);
+                tempLine = rd.readLine();
+            }
+            responseContent = tempStr.toString();
+            System.out.println(responseContent);
+            rd.close();
+            in.close();
+        } catch (IOException e) {
+            System.out.println("请求错信息:"+e.getMessage());
+        } finally {
+            if (url_con != null) {
+                url_con.disconnect();
+            }
+        }
+        return responseContent;
+    }
+
+    public static void main(String[] args) {
+        doPostFile();
+    }
 }
+
